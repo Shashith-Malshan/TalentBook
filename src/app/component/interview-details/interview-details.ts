95b71beb5b09f92517ef5877fda6682a9251fd46
Shashith-Malshan <malshanshashith318@gmail.com>
2026-02-18 12:09:33 +0530

    add interview-detail component

---
+++ src/app/component/interview-details/interview-details.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-interview-details',
+  imports: [],
+  templateUrl: './interview-details.html',
+  styleUrl: './interview-details.css',
+})
+export class InterviewDetails {
+
+}
